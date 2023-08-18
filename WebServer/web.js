69e6c16bf6c8d8f1d533c295158c642db536a1e4
koimasshiro <ese.imhariagbe@gmail.com>
2023-08-18 21:22:25 +0100

    initial commit

---
+++ WebServer/web.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const fs = require("fs");
+const http = require("http");
+
+const webPage = path.join(__dirname, 'index.html');
+const errorPage = path.join(__dirname, 'errorPage.html');
+
+
+const PORT = 3000;
+
+
+
+function reqHandler(req, res){
+    if(req.url === '/'){
+        getHomePage(req, res);
+    }
+
+    if(req.url.endswith(".html") && req.method === "GET"){
+        try{
+            getRequestedPage(req,res);
+        }
+        catch{
+            getErrorPage(req,res);
+        }
+    }
+}
+
+const server = http.createServer(reqHandler);
+
+server.listen(PORT, ()=> {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
+
+
+function getHomePage(req, res){
+    res.setHeader("content-type", "text/html");
+    res.writeHead(200);
+    res.end(fs.readFileSync(webPage));
+}
+
+function getRequestedPage(req, res){
+    const file = req.url.split('/')[1];
+    const mainPath = path.join(__dirname, file);
+    const webPath = fs.readFileSync(mainPath);
+
+    res.setHeader("content-type", "text/html");
+    res.writeHead(200);
+    res.end(webPath);
+}
+
+function getErrorPage(req, res){
+    res.setHeader("content-type","text/html");
+    res.writeHead(404);
+    res.end(fs.readFileSync(errorPage));
+}
